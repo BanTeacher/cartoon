01d57087b55e6079a2d50a05a7c50462fa726b27
BanTeacher <89731197+BanTeacher@users.noreply.github.com>
2025-09-08 10:40:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     1: true,
     2: true,
     3: true,
-    4: false,
-    5: false,
+    4: true,
+    5: true,
     6: false,
     7: false,
   };
@@ -118,5 +118,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
 
 
